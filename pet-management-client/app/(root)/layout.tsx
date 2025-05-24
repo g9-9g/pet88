ae0c9b8be2e2071e64d967f68f391e4c21bf9568
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2025-05-24 16:05:31 +0700

    fix UserProvider issue

--- pet-management-client/app/(root)/layout.tsx
+++ pet-management-client/app/(root)/layout.tsx
@@ -1,22 +1,19 @@
 import { Toaster } from "@/components/ui/sonner";
-import { UserProvider } from "@/context/UserContext";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <UserProvider>
-      <>
-        {children}
-        <Toaster
-          toastOptions={{
-            className: "!text-sm",
-            classNames: {
-              success: "!bg-green-500 !text-white",
-              error: "!bg-brand !text-white",
-            },
-          }}
-        />
-      </>
-    </UserProvider>
+    <>
+      {children}
+      <Toaster
+        toastOptions={{
+          className: "!text-sm",
+          classNames: {
+            success: "!bg-green-500 !text-white",
+            error: "!bg-brand !text-white",
+          },
+        }}
+      />
+    </>
   );
 };
 
